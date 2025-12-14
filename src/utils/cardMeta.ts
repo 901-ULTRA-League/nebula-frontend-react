eb90329ee6bb245c75bd86d5ea63d1151ece0d1c
Natalya Higdon <queenlinuxtechgroup@gmail.com>
2025-12-13 22:07:23 -0600

    fix to alt mebius

--- src/utils/cardMeta.ts
+++ src/utils/cardMeta.ts
@@ -1,7 +1,7 @@
 import type { Card } from "../types";
 
 const SEMI_TRANSCENDENT_NUMBERS = new Set(["PR-107"]);
-const TRANSCENDENT_NUMBERS = new Set(["BP01-022", "AP(06/20)BP01-022", "BP01-026"].map((n) => n.toUpperCase()));
+const TRANSCENDENT_NUMBERS = new Set(["BP01-022", "AP(06/20) BP01-022", "BP01-026"].map((n) => n.toUpperCase()));
 const NO_LIMIT_NUMBERS = new Set(["PR-036", "PR-107"]);
 
 const normalizeNumber = (card: Card) => card.number?.toUpperCase();
